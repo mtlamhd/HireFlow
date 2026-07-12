7452b9373ab12610a56e48b7f7164763ed959c79
mtlamhd <mehradmtla777@gmail.com>
2026-07-12 18:37:54 +0330

    feat/add-generic-repo

---
+++ HireFlow.Domain/Interfaces/Repo/IGenericRepository.cs
@@ -0,0 +1,25 @@
+using System.Linq.Expressions;
+using HireFlow.Domain.Abstractions;
+using HireFlow.Domain.Entities;
+
+namespace HireFlow.Domain.Interfaces.Repo;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{   
+    Task<T?> GetByIdAsync(Guid id, bool tracking = false);
+    
+    Task<T?> GetFirstOrDefaultAsync(Expression<Func<T, bool>> predicate, bool tracking = false);
+    
+    Task<List<T>> QueryAsync(Expression<Func<T, bool>> predicate, Paging paging, bool tracking = false);
+    
+    Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+    
+    Task AddAsync(T entity);
+    
+    void Update(T entity);
+    
+    void Delete(T entity);
+    
+    void SoftDelete(T entity, Guid requesterId);
+    
+}
---
+++ HireFlow.Infrustructure/Repositories/GenericRepository.cs
@@ -0,0 +1,86 @@
+using System.Linq.Expressions;
+using HireFlow.Domain.Abstractions;
+using HireFlow.Domain.Entities;
+using HireFlow.Domain.Interfaces.Repo;
+using Microsoft.EntityFrameworkCore;
+
+namespace HireFlow.Infrustructure.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    protected readonly DbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public GenericRepository(DbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+    public async Task<T?> GetByIdAsync(Guid id, bool tracking = false)
+    {
+        var query = _dbSet.AsQueryable();
+
+        if (!tracking)
+        {
+            query = query.AsNoTracking();
+        }
+
+        return await query.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<T?> GetFirstOrDefaultAsync(Expression<Func<T, bool>> predicate, bool tracking = false)
+    {
+        var query = _dbSet.AsQueryable();
+
+        if (!tracking)
+        {
+            query = query.AsNoTracking();
+        }
+
+        return await query.FirstOrDefaultAsync(predicate);
+    }
+
+    public async Task<List<T>> QueryAsync(Expression<Func<T, bool>> predicate, Paging paging, bool tracking = false)
+    {
+        var query = _dbSet.AsQueryable();
+
+        if (!tracking)
+        {
+            query = query.AsNoTracking();
+        }
+
+        return await query
+            .Where(predicate)
+            .OrderByDescending(x => x.CreatedAt)
+            .Skip(paging.Skip)
+            .Take(paging.PageSize)
+            .ToListAsync();
+    }
+
+    public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.AnyAsync(predicate);
+    }
+
+   
+    public async Task AddAsync(T entity) 
+    {
+        await _dbSet.AddAsync(entity);
+    }
+    
+
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+
+    public void SoftDelete(T entity, Guid requesterId)
+    {
+        entity.SetAsDeleted(requesterId);
+    }
+}
